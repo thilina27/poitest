d2fe4f0c7872b484034aae8b0cf7bd50548a52b6
Thilina27 <madhusanka.thilina@gmail.com>
2016-05-31 18:15:17 +0530

    test1

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -5,12 +5,27 @@ import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.apache.poi.xslf.usermodel.XSLFFreeformShape;
+
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
 
 public class test1 {
 
-    public static void main(String arg[]){
-        System.out.println("dsdsdwsdsd ");
+    public static void main(String arg[]) throws IOException {
+        //creating a new empty slide show
+        XMLSlideShow ppt = new XMLSlideShow();
+
+        //creating an FileOutputStream object
+        File file =new File("example1.pptx");
+        FileOutputStream out = new FileOutputStream(file);
+
+        //saving the changes to a file
+        ppt.write(out);
+        System.out.println("Presentation created successfully");
+        out.close();
     }
 
 
